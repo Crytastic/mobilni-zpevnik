5bb497f1c90cc9d149348016febf61bf2ea8e179
Maxmilián Šeffer <max.seffer@gmail.com>
2023-12-24 15:16:05 +0100

    adjust home and search tab, sort alphabetically, add simple search bar

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:localization/localization.dart';
 import 'package:mobilni_zpevnik/models/song.dart';
 import 'package:mobilni_zpevnik/screens/song_screen.dart';
-
 import 'package:mobilni_zpevnik/service/song_service.dart';
 
 class HomeTab extends StatefulWidget {
@@ -15,10 +13,6 @@ class HomeTab extends StatefulWidget {
 
 class _HomeTabState extends State<HomeTab> {
   final _songService = GetIt.I<SongService>();
-  final _titleController = TextEditingController();
-  final _artistController = TextEditingController();
-  String? _songTitle;
-  String? _songArtist;
 
   @override
   Widget build(BuildContext context) {
@@ -37,13 +31,16 @@ class _HomeTabState extends State<HomeTab> {
         }
 
         final songs = snapshot.data!;
+        songs.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
 
         if (songs.isEmpty) {
           return const Center(child: Text('No songs available.'));
         }
 
         return ListView.builder(
-          padding: const EdgeInsets.all(10),
+          itemCount: songs.length,
           itemBuilder: (context, index) {
             final song = songs[index];
 
@@ -60,7 +57,6 @@ class _HomeTabState extends State<HomeTab> {
               },
             );
           },
-          itemCount: songs.length,
         );
       },
     );
--- lib/tabs/search_tab.dart
+++ lib/tabs/search_tab.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:localization/localization.dart';
+import 'package:mobilni_zpevnik/models/song.dart';
+import 'package:mobilni_zpevnik/screens/song_screen.dart';
+import 'package:mobilni_zpevnik/service/song_service.dart';
 
-class SearchTab extends StatelessWidget {
+class SearchTab extends StatefulWidget {
   const SearchTab({Key? key}) : super(key: key);
+
+  @override
+  State<SearchTab> createState() => _SearchTabState();
+}
+
+class _SearchTabState extends State<SearchTab> {
+  final TextEditingController _searchController = TextEditingController();
+  final _songService = GetIt.I<SongService>();
+  List<Song> _songs = [];
+  List<Song> _filteredSongs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _songService.songsStream.listen((List<Song> songs) {
+      setState(() {
+        _songs = songs;
+        _songs.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        _onSearchChanged(_searchController
+            .text); // Update filtered songs when the original list changes
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,14 +39,51 @@ class SearchTab extends StatelessWidget {
     final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
     final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
 
-    return ListView.builder(
-      itemCount: 25,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          tileColor: index.isOdd ? oddItemColor : evenItemColor,
-          title: Text('${'song-suggestion'.i18n()} $index'),
-        );
-      },
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _searchController,
+            decoration: const InputDecoration(
+              hintText: 'Search',
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: (query) => _onSearchChanged(query),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _filteredSongs.length,
+            itemBuilder: (BuildContext context, int index) {
+              final song = _filteredSongs[index];
+
+              return ListTile(
+                tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                title: Text('${song.name} - ${song.artist}'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SongScreen(song: song),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
     );
+  }
+
+  void _onSearchChanged(String query) {
+    setState(() {
+      _filteredSongs = _songs
+          .where((song) =>
+              song.name.toLowerCase().contains(query.toLowerCase()) ||
+              song.artist.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
   }
 }
